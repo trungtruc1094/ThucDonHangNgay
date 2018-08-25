8544ccfeaa933f95760aef906808afda8556b6bf
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-08-25 23:58:48 +1200

    commit

--- app/src/main/java/com/example/trungtrucnguyen/thucdonhangngay/fragments/CardFragment.java
+++ app/src/main/java/com/example/trungtrucnguyen/thucdonhangngay/fragments/CardFragment.java
@@ -1,5 +1,7 @@
 package com.example.trungtrucnguyen.thucdonhangngay.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
@@ -9,10 +11,12 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.trungtrucnguyen.thucdonhangngay.CardAdapter;
+import com.example.trungtrucnguyen.thucdonhangngay.FoodDetailsActivity;
 import com.example.trungtrucnguyen.thucdonhangngay.R;
 
 
 public class CardFragment extends Fragment {
+    private Activity context;
     private static final String POSITION = "POSITION";
     private int pos;
     private CardView cardView;
@@ -32,6 +36,7 @@ public class CardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.item_viewpager, container, false);
+        this.context = getActivity();
 
         // Refer to cardView of fragment's layout
         cardView = (CardView) view.findViewById(R.id.cardView);
@@ -41,6 +46,22 @@ public class CardFragment extends Fragment {
         foodTitle.setText(String.format("Canh kho qua %d", pos));
 
         return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Create an Intent object
+                Intent intent=new Intent(context, FoodDetailsActivity.class);
+
+                //Start the second activity
+                startActivity(intent);
+            }
+        });
     }
 
     public static CardFragment getInstance(int position){
