221f84777dbec7d35b20e5ed1f0803a444637258
Trung Truc <1259035@itec.hcmus.edu.vn>
2018-08-26 14:38:37 +1200

    commit

--- app/src/main/java/com/example/trungtrucnguyen/thucdonhangngay/FoodDetailsActivity.java
+++ app/src/main/java/com/example/trungtrucnguyen/thucdonhangngay/FoodDetailsActivity.java
@@ -6,7 +6,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class FoodDetailsActivity extends AppCompatActivity {
 
@@ -15,6 +21,38 @@ public class FoodDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_details);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Detail");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_food_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.favorite) {
+            LinearLayout favoriteBtn = (LinearLayout) item.getActionView().findViewById(R.id.navigationItem);
+            if (favoriteBtn.isChecked()){
+                Toast.makeText(FoodDetailsActivity.this, "Love it!", Toast.LENGTH_LONG).show();
+                return true;
+            } else {
+                Toast.makeText(FoodDetailsActivity.this, "No love it!", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
